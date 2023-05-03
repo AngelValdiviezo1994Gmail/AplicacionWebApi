ac9e8c6f5b5cff95e5e2de81778132a12c7b5b36
AngelValdiviezo1994Gmail <131912926+AngelValdiviezo1994Gmail@users.noreply.github.com>
2023-05-02 22:23:44 -0500

    Primer respaldo

---
+++ AplicacionWebApiAngelValdiviezo/Domain/Entities/VentaEntradas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class VentaEntradas
+    {
+        public DateTime FechaEvento { get; set; }
+        public string LugarEvento { get; set; }
+        public int NumEntrada { get; set; }
+        public string DescripcionEvento { get; set; }
+        public int PrecioEvento { get; set; }
+    }
+}
